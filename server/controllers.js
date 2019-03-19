ad6e0b38efebf85cafac6424ba55a1144cdb9179
Jennifer Haren <jennifer.haren110@gmail.com>
2019-03-18 20:23:27 -0700

    Add get request from Lyft API
    Added route and controller to handle get requests for Lyft pricing.  Get request sends starting and ending coordinates to Lyft's public API, and sends back the data in the express response object.  Get request also uses client token for authentication.

---
+++ server/controllers.js
@@ -0,0 +1,34 @@
+const {lyftToken} = require('../config.js');
+const lyft = require('node-lyft');
+let lyftData;
+
+// LYFT API AUTHORIZATION
+const defaultClient = lyft.ApiClient.instance;
+defaultClient.authentications['Client Authentication'].accessToken = lyftToken;
+
+module.exports = {
+  lyft: {
+    get: (req, res) => {
+
+      // const {startLat, startLng, endLat, endLng} = req.params;
+      const lyftPublicApi = new lyft.PublicApi();
+
+      let startLat = 33.9626;
+      let startLng = -118.3988;
+
+      let opts = {
+        'endLat': 33.8366,
+        'endLng': -117.9143
+      };
+
+      lyftPublicApi.getCost(startLat, startLng, opts)
+      .then((data) => {
+        res.status(200).send(data)
+      })
+      .catch(err => {
+        res.status(404).send('Error getting data', err)
+      });
+    }
+  }
+};
+
