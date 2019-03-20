807e68ced3b3a399e9123dcb3d39c6e1db2b2f43
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-03-20 12:06:59 -0700

    Spell out API endpoints in routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,35 @@
 const router = require('express').Router();
 const controllers = require('./controllers.js');
 
+/*
+  Apis to keep in mind
+    authentication:
+      -get - verify that user exists
+        -params: { username, password }
+        -response:  statusCode: 200 //good
+                    statusCode: 403 //bad
+      -post - create a new user
+        -params: { username, password }
+        -response:  statusCode: 201 //good
+                    statusCode: 403 //bad
+    scenarios:
+      -get - get all the saved scenarios for that user
+        -params: { username }
+        -response:  statusCode: 200, an array of objects with
+                    the scenarios they saved in the db //good
+                    statusCode: 404 //bad, no saved scenarios
+      -post - save the current scenario in the db
+        -params: { username, prices, startCity, endCity, scenarioName }
+        -response:  statusCode: 201 //good
+                    statusCode: 403 //bad
+    prices:
+      -get - get all the prices from the rideshare apis
+        -params: { startCity, endCity }
+        -response:  statusCode 200
+                    body = { startCoord, endCoord, prices } //good
+                    statusCode 404 //bad city doesnt exist
+*/
+
 router
   .route('/lyft')
   .get(controllers.lyft.get);
