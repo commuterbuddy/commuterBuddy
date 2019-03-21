b099799603e024324d0b651b1d25c650b3a1b4ae
Jun Kelly <kellyja4@gmail.com>
2019-03-21 15:10:53 -0700

    Root page routes to login, proper /results route in navbar

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation.jsx';
 import Login from '../Login/Login.jsx';
+import Results from '../Results.jsx';
 import History from '../History/History.jsx';
 
 class App extends Component {
@@ -26,7 +27,7 @@ class App extends Component {
         <Navigation authenticated={authenticated} />
         <Switch>
           <Route path="/" exact render={() => <Login authenticate={this.handleAuthenticate} authenticated={authenticated}/>}  />
-          {/* <Route path='/results' component={ Results } /> */}
+          <Route path='/results' component={Results} />
           <Route path='/history' render={() => <History />} />
         </Switch>
       </BrowserRouter>
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Results from './components/Results.jsx';
+import App from './components/App/App.jsx';
 
-ReactDOM.render(<Results />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
