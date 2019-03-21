9b423c8bbdfb13f6fc96862e92e81f52b3db0e69
Jun Kelly <kellyja4@gmail.com>
2019-03-21 11:42:52 -0700

    History skeleton

---
+++ client/src/components/History/History.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styles from './History.css';
+
+class History extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This is a placeholder.
+      </div>
+    );
+  }
+}
+
+export default History;
