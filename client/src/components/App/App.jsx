30864aa9d8f6f80cc6bba1bc40af284366322998
Jun Kelly <kellyja4@gmail.com>
2019-03-21 13:19:41 -0700

    Removed requests temporarily

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation.jsx';
 import Login from '../Login/Login.jsx';
+import History from '../History/History.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       authenticated: false
-    }
+    };
+    this.handleAuthenticate = this.handleAuthenticate.bind(this);
   }
-  
+
+  handleAuthenticate(status) {
+    this.setState({
+      authenticated: status
+    });
+  }
+
   render() {
+    let { authenticated } = this.state;
     return (
       <BrowserRouter>
-        <Navigation authenticated={this.state.authenticated} />
+        <Navigation authenticated={authenticated} />
         <Switch>
-          <Route path='/' exact component={ Login } />
-          {/* <Route path='/results' component={ Results } />
-          <Route path='/history' component={ History } /> */}
+          <Route path="/" exact render={() => <Login authenticate={this.handleAuthenticate} authenticated={authenticated}/>}  />
+          {/* <Route path='/results' component={ Results } /> */}
+          <Route path='/history' render={() => <History />} />
         </Switch>
       </BrowserRouter>
     );
