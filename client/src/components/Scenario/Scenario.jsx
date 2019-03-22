730afc3650938a518772217880381d4660df4e68
Jun Kelly <kellyja4@gmail.com>
2019-03-22 10:18:06 -0700

    Updated prop names and added minimal formatting

--- client/src/components/Scenario/Scenario.jsx
+++ client/src/components/Scenario/Scenario.jsx
@@ -2,48 +2,55 @@ import React from 'react';
 import styles from './Scenario.css';
 
 const Scenario = ({ scenario }) => (
-  <div className={styles.container}>
-    <div className={styles.flexTable}>
-      <div className={styles.category}>
-        {scenario.scenarioName}
-      </div>
-      <div>
-        Start:
-      </div>
-      <div>
-        End: 
-      </div>
-      </div>
-    <div className={styles.flexTable}>
-      Cost (per gallon): {scenario.costPerGallon}
-      <br />
-      Daily gas cost: {scenario.dailyGasCost}
-      <br />
-      Bird price: {scenario.birdPrice}
+  <div className={styles.mainContainer}>
+    <div className={`${styles.category} ${styles.title}`}>
+      {scenario.tripName}
     </div>
-    <div className={styles.flexTable}>
-      <div className={styles.category}>
-        Uber
+    <div className={styles.flexContainer}>
+      <div className={styles.flexTable}>
+        <div className={styles.align}>
+          Start: {scenario.startCity}
+          <br />
+          End: {scenario.endCity}
+        </div>
       </div>
-      {scenario.uberRides.map((uber, i) => {
-        return (
-          <div key={i}>
-            <div>{uber.name}: {uber.price}</div>
-          </div>
-        );
-      })}
-    </div>
-    <div className={styles.flexTable}>
-      <div className={styles.category}>
-        Lyft
+      <div className={styles.flexTable}>
+        <div className={styles.align}>
+          Cost (per gallon): {scenario.costPerGallon}
+          <br />
+          Daily gas cost: {scenario.dailyGasCost}
+          <br />
+          Bird price: {scenario.birdPrice}
+        </div>
       </div>
-      {scenario.lyftRides.map((lyft, i) => {
-        return (
-          <div key={i}>
-            <div>{lyft.name}: {lyft.price}</div>
+      <div className={styles.flexTable}>
+        <div className={styles.align}>
+          <div className={styles.category}>
+            Uber
           </div>
-        );
-      })}
+          {scenario.uberRides.map((uber, i) => {
+            return (
+              <div key={i}>
+                <div>{uber.name}: {uber.price}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.flexTable}>
+        <div className={styles.align}>
+          <div className={styles.category}>
+            Lyft
+          </div>
+          {scenario.lyftRides.map((lyft, i) => {
+            return (
+              <div key={i}>
+                <div>{lyft.name}: {lyft.price}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   </div>
 );
