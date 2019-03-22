95db329685fd3d5ce1f518a14ce3276602bdac8b
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-03-22 16:22:24 -0700

    Fix typos

--- server/controllers.js
+++ server/controllers.js
@@ -1,9 +1,8 @@
 const getPrice = require('./apis/price');
-const dummyData = require('./dummyData.js')
-;
+const dummyData = require('./dummyData.js');
+
 module.exports = {
   getPrices: (req, res) => {
-    console.log(req.body, req.query, req.params, req.data)
     const { startCity, endCity, mpg } = req.query;
     // const startCity = 'corona';
     // const endCity = 'anaheim';
@@ -14,11 +13,10 @@ module.exports = {
         res.status(200).send(price);
       })
       .catch((err) => {
-        console.log(err, 'in controller');
         res.status(404).send(err);
       });
   },
   getScenarios: (req, res) => {
-    res.status(200).send(dummyData)
-  }
+    res.status(200).send(dummyData);
+  },
 };
