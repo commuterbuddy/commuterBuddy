99428ea5cfaa8de2786c2056fe98bb45764e6e94
Jennifer Haren <jennifer.haren110@gmail.com>
2019-03-21 19:13:58 -0700

    Move functionality to parent component
    Moved all functions to parent component so all changes and results from get request are stored in parent component's state.  Added function getMidpoint to center map each time a new commute is submitted.

--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -15,78 +15,9 @@ class UserForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      homeCounty: '',
-      workCounty: '',
-      startCity: '',
-      endCity: '',
-      mpg: 0,
-      distance: 0,
-      lyftRides: [],
-      uberRides: [],
-      birdPrice: 0,
-      dailyGasCost: 0,
-      costPerGallon: 0
-    }
-    this.handleHomeChange = this.handleHomeChange.bind(this);
-    this.handleWorkChange = this.handleWorkChange.bind(this);
-    this.handleGasChange = this.handleGasChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  handleGasChange(event) {
-    let mpg = parseInt(event.target.value);
-    this.setState({
-      mpg: mpg
-    });
-  }
-
-  handleHomeChange(event) {
-    if (this.state.homeCounty.length > 1) {
-      this.setState({
-        startCity: event.target.value
-      })
-    } else {
-      this.setState({
-        homeCounty: event.target.value
-      })
     }
   }
-
-  handleWorkChange(event) {
-    if (this.state.workCounty.length > 1) {
-      this.setState({
-        endCity: event.target.value
-      })
-    } else {
-      this.setState({
-        workCounty: event.target.value
-      })
-    }
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const {startCity, endCity, mpg} = this.state;
-
-    console.log('THESE ARE THE PARAMS------------', startCity, endCity, mpg);
-
-    axios
-      .get('/api/prices', {params: {startCity, endCity, mpg}})
-      .then(({data}) => {
-        this.setState({
-          lyftRides: data.lyftRides,
-          uberRides: data.uberRides,
-          distance: data.distance,
-          birdPrice: data.birdPrice,
-          dailyGasCost: data.dailyGasCost,
-          costPerGallon: data.costPerGallon
-        })
-      })
-      .catch(err => {
-        console.log('Error getting data', err)
-      });
-  }
-
 
   render() {
     return (
@@ -94,7 +25,7 @@ class UserForm extends Component {
         <form>
           <label>
             Home:
-            <select id="homeCounty" onChange={this.handleHomeChange}>
+            <select id="homeCounty" onChange={this.props.handleHomeChange}>
               <option value="Start">Choose your county</option>
               <option value="alameda" >Alameda</option>
               <option value="los angeles" >Los Angeles</option>
@@ -111,22 +42,22 @@ class UserForm extends Component {
           
           <label>
             City:
-            {this.state.homeCounty === 'alameda' ? <Alameda change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'los angeles' ? <LosAngeles change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'orange' ? <Orange change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'riverside' ? <Riverside change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'sacramento' ? <Sacramento change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'san bernardino' ? <SanBernardino change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'san diego' ? <SanDiego change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'san francisco' ? <SanFrancisco change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'santa barbara' ? <SantaBarbara change={this.handleHomeChange} /> :
-            this.state.homeCounty === 'santa clara' ? <SantaClara change={this.handleHomeChange} /> : null}
+            {this.props.homeCounty === 'alameda' ? <Alameda change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'los angeles' ? <LosAngeles change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'orange' ? <Orange change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'riverside' ? <Riverside change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'sacramento' ? <Sacramento change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'san bernardino' ? <SanBernardino change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'san diego' ? <SanDiego change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'san francisco' ? <SanFrancisco change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'santa barbara' ? <SantaBarbara change={this.props.handleHomeChange} /> :
+            this.props.homeCounty === 'santa clara' ? <SantaClara change={this.props.handleHomeChange} /> : null}
           </label>
 
           <br /><br />
           <label>
             Work:
-              <select id="workCounty" onChange={this.handleWorkChange}>
+              <select id="workCounty" onChange={this.props.handleWorkChange}>
                 <option value="Start">Choose your county</option>
                 <option value="alameda">Alameda</option>
                 <option value="los angeles">Los Angeles</option>
@@ -143,16 +74,16 @@ class UserForm extends Component {
 
           <label>
             City:
-            {this.state.workCounty === 'alameda' ? <Alameda change={this.handleWorkChange} /> :
-            this.state.workCounty === 'los angeles' ? <LosAngeles change={this.handleWorkChange} /> :
-            this.state.workCounty === 'orange' ? <Orange change={this.handleWorkChange} /> :
-            this.state.workCounty === 'riverside' ? <Riverside change={this.handleWorkChange} /> :
-            this.state.workCounty === 'sacramento' ? <Sacramento change={this.handleWorkChange} /> :
-            this.state.workCounty === 'san bernardino' ? <SanBernardino change={this.handleWorkChange} /> :
-            this.state.workCounty === 'dan diego' ? <SanDiego change={this.handleWorkChange} /> :
-            this.state.workCounty === 'san francisco' ? <SanFrancisco change={this.handleWorkChange} /> :
-            this.state.workCounty === 'santa barbara' ? <SantaBarbara change={this.handleWorkChange} /> :
-            this.state.workCounty === 'santa clara' ? <SantaClara change={this.handleWorkChange} /> : null}
+            {this.props.workCounty === 'alameda' ? <Alameda change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'los angeles' ? <LosAngeles change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'orange' ? <Orange change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'riverside' ? <Riverside change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'sacramento' ? <Sacramento change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'san bernardino' ? <SanBernardino change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'dan diego' ? <SanDiego change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'san francisco' ? <SanFrancisco change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'santa barbara' ? <SantaBarbara change={this.props.handleWorkChange} /> :
+            this.props.workCounty === 'santa clara' ? <SantaClara change={this.props.handleWorkChange} /> : null}
           </label>
 
           <br /><br />
@@ -160,9 +91,9 @@ class UserForm extends Component {
 
           <label>
             Average Miles Per Gallon (MPG):
-            <input type="text" name="mpg" onChange={this.handleGasChange} /><br />
+            <input type="text" name="mpg" onChange={this.props.handleGasChange} /><br />
           </label><br />
-          <input type="submit" value="Find my commute!" onClick={this.handleSubmit} />
+          <input type="submit" value="Find my commute!" onClick={(e) => this.props.submit(e)} />
         </form>
       </div>
 
