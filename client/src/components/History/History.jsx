850f53d2f9fb0d637b3996ea5abb92e762ddeb0d
Jun Kelly <kellyja4@gmail.com>
2019-03-21 19:35:39 -0700

    Basic foundation to display scenarios and an axios get request with username params

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import styles from './History.css';
+import Scenario from '../Scenario/Scenario.jsx';
 
 class History extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    let user = localStorage.getItem('user');
+    axios
+      .get("/api/scenarios", {params: {username: user}})
+      .then(({data}) => {
+        this.setState({
+          data: [...data]
+        });
+      })
+      .catch(err => console.log(err))
   }
 
   render() {
     return (
-      <div>
-        This is a placeholder.
+      <div className={styles.container}>
+        <div>
+          <h2>Jun's Commute History</h2>
+          {this.state.data.map((scenario, i) => <Scenario key={i} scenario={scenario} />)}
+        </div>
       </div>
     );
   }
