d2bd1192d68f999d8c33fb2294c9cc901f99a2d3
Jun Kelly <kellyja4@gmail.com>
2019-03-21 20:59:42 -0700

    Temporarily added dummy data and routes for History component

---
+++ server/dummyData.js
@@ -0,0 +1,152 @@
+module.exports = [
+  {
+    "startCoords": {
+      "startLat": 37.4419,
+      "startLng": -122.143
+    },
+    "endCoords": {
+      "endLat": 37.8044,
+      "endLng": -122.2711
+    },
+    "birdPrice": "$68",
+    "distance": 71.52,
+    "uberRides": [
+      {
+        "name": "UberX",
+        "price": "$136"
+      },
+      {
+        "name": "Select",
+        "price": "$298"
+      },
+      {
+        "name": "UberXL",
+        "price": "$206"
+      },
+      {
+        "name": "Black",
+        "price": "$386"
+      },
+      {
+        "name": "Black SUV",
+        "price": "$446"
+      },
+      {
+        "name": "Assist",
+        "price": "$136"
+      },
+      {
+        "name": "WAV",
+        "price": "$136"
+      },
+      {
+        "name": "UberPool",
+        "price": "$130"
+      }
+    ],
+    "lyftRides": [
+      {
+        "name": "Shared",
+        "price": "$120"
+      },
+      {
+        "name": "Lyft",
+        "price": "$140"
+      },
+      {
+        "name": "Lyft XL",
+        "price": "$210"
+      },
+      {
+        "name": "Lux",
+        "price": "$320"
+      },
+      {
+        "name": "Lux Black",
+        "price": "$420"
+      },
+      {
+        "name": "Lux Black XL",
+        "price": "$470"
+      }
+    ],
+    "dailyGasCost": "$13",
+    "costPerGallon": 3.542,
+    "scenarioName": "My expensive commute"
+   },
+   {
+    "startCoords": {
+      "startLat": 37.4419,
+      "startLng": -122.143
+    },
+    "endCoords": {
+      "endLat": 37.8044,
+      "endLng": -122.2711
+    },
+    "birdPrice": "$68",
+    "distance": 71.52,
+    "uberRides": [
+      {
+        "name": "UberX",
+        "price": "$136"
+      },
+      {
+        "name": "Select",
+        "price": "$298"
+      },
+      {
+        "name": "UberXL",
+        "price": "$206"
+      },
+      {
+        "name": "Black",
+        "price": "$386"
+      },
+      {
+        "name": "Black SUV",
+        "price": "$446"
+      },
+      {
+        "name": "Assist",
+        "price": "$136"
+      },
+      {
+        "name": "WAV",
+        "price": "$136"
+      },
+      {
+        "name": "UberPool",
+        "price": "$130"
+      }
+    ],
+    "lyftRides": [
+      {
+        "name": "Shared",
+        "price": "$120"
+      },
+      {
+        "name": "Lyft",
+        "price": "$140"
+      },
+      {
+        "name": "Lyft XL",
+        "price": "$210"
+      },
+      {
+        "name": "Lux",
+        "price": "$320"
+      },
+      {
+        "name": "Lux Black",
+        "price": "$420"
+      },
+      {
+        "name": "Lux Black XL",
+        "price": "$1"
+      }
+    ],
+    "dailyGasCost": "$69",
+    "costPerGallon": 4.20,
+    "scenarioName": "Another expensive commute"
+   }
+]
