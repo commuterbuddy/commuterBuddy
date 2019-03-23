94beff4a92284c26fb0f9d56527ca84e0aa7b705
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-03-23 09:32:19 -0700

    Install basic graphql dependencies and pg and sequelize and make DB filestructure

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const path = require('path');
 const cors = require('cors');
 const express = require('express');
 const parser = require('body-parser');
+
 const routes = require('./routes.js');
+
+
+// const db = require('../database');
 
 const PORT = 3000;
 
@@ -11,8 +15,10 @@ const app = express();
 app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.use('/api', routes);
+
 
 app.listen(PORT, () => console.log('App is listening on PORT', PORT));
