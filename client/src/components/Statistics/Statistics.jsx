d019b9fb9512da9bd62e0ecdc6ecb95ce40dde38
Jennifer Haren <jennifer.haren110@gmail.com>
2019-04-29 11:53:18 -0700

    Refactor UserForm and Statistics to be Functional
    Refactored UserForm and Statistics components to be Functional components, since they need to render changes to parent Results component, which sends post/get requests, etc.

--- client/src/components/Statistics/Statistics.jsx
+++ client/src/components/Statistics/Statistics.jsx
@@ -1,58 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Input from '../factoryComponents/Input.jsx';
 import Button from '../factoryComponents/Button.jsx';
 import Card from '../factoryComponents/Card.jsx';
 import ListCard from '../factoryComponents/ListCard.jsx';
 import styles from './OptionStyles.css';
 
-class Statistics extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
+const Statistics = (props) => {
 
-  render() {
+  const { button, className, carImg, carPrice, birdImg, birdPrice, lyftImg, lyftRides, uberImg, uberRides, handleInputChange, tripSubmit, tripSubmitted } = props;
 
-    const { button, className, carImg, carPrice, birdImg, birdPrice, lyftImg, lyftRides, uberImg, uberRides, handleInputChange, tripSubmit, tripSubmitted } = this.props;
-
-    const { buttonStyle, card, carImgStyle, carContainer, options, item, img, container, name, price, flexInput, border, flexSubmit, textInput } = styles;
+  const { buttonStyle, card, carImgStyle, carContainer, options, item, img, container, name, price, flexInput, border, flexSubmit, textInput } = styles;
     
-    return (
+  return (
       
-      <div className={className.statsContainer}>
+    <div className={className.statsContainer}>
         
-        <div className={className.statsFlex}>
+      <div className={className.statsFlex}>
 
-          <Card cardType="car" cardStyle={card} imgStyle={carImgStyle} itemStyle={item} src={carImg} alt="CarImg" containerStyle={carContainer} optionsStyle={options} price={carPrice} />
-          <Card cardType="bird" cardStyle={card} imgStyle={img} itemStyle={item} src={birdImg} alt="BirdLogo" containerStyle={container} optionsStyle={options} price={birdPrice} />          
-          <ListCard cardStyle={card} imgStyle={img} itemStyle={item} src={lyftImg} alt="LyftLogo" containerStyle={container} optionsStyle={options} rides={lyftRides} />          
-          <ListCard cardStyle={card} imgStyle={img} itemStyle={item} src={uberImg} alt="UberLogo" containerStyle={container} optionsStyle={options} rides={uberRides} />
+        <Card cardType="car" cardStyle={card} imgStyle={carImgStyle} itemStyle={item} src={carImg} alt="CarImg" containerStyle={carContainer} optionsStyle={options} price={carPrice} />
+        <Card cardType="bird" cardStyle={card} imgStyle={img} itemStyle={item} src={birdImg} alt="BirdLogo" containerStyle={container} optionsStyle={options} price={birdPrice} />          
+        <ListCard cardStyle={card} imgStyle={img} itemStyle={item} src={lyftImg} alt="LyftLogo" containerStyle={container} optionsStyle={options} rides={lyftRides} />          
+        <ListCard cardStyle={card} imgStyle={img} itemStyle={item} src={uberImg} alt="UberLogo" containerStyle={container} optionsStyle={options} rides={uberRides} />
             
-            {carPrice.costPerGallon ? 
-            <div className={flexInput}>
+        {carPrice.costPerGallon ? 
+        <div className={flexInput}>
 
-              <div className={border}>
+          <div className={border}>
             
-                <div>
-                  <h2>Save this commute?</h2>
-                </div>  
+            <div>
+              <h2>Save this commute?</h2>
+            </div>  
               
-                <div className={flexSubmit}>
-                  <Input className={textInput} id='tripName' type='text' placeholder='Name your trip' handleChange={handleInputChange} />  
-                  <Button className={buttonStyle} id="Save" submitFunc={tripSubmit} tripSubmitted={tripSubmitted} />                                 
-                </div>
+            <div className={flexSubmit}>
+              <Input className={textInput} id='tripName' type='text' placeholder='Name your trip' handleChange={handleInputChange} />  
+              <Button className={buttonStyle} id="Save" submitFunc={tripSubmit} tripSubmitted={tripSubmitted} />                                 
+            </div>
             
-              </div>
+          </div>
             
-            </div> 
-             : null}
+        </div> 
+        : null}
         
-        </div>
-  
       </div>
-    )
-  }
+  
+    </div>
+  )
 }
 
 export default Statistics;
